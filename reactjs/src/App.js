43e34490dcbf24688a389617db905e2935e199b0
RishanSubagar <rishans48@gmail.com>
2024-03-02 21:51:25 -0500

    Change UI

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import CameraComponent from './CameraComponent';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className ="App">
-    
-      <h1>Insta Recipe</h1>
+
+      <h1>InstaRecipe</h1>
       <CameraComponent /> {/* Use the CameraComponent */}
     </div>
   );
